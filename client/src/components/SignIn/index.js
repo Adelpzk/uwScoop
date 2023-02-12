126ff3197c7f1c4419569fe29f9d406105c821c4
Michael Sheng <m3sheng@uwaterloo.ca>
2023-02-11 23:24:46 -0500

    Added password verification and error handling

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -20,263 +20,290 @@ import FormControl from '@mui/material/FormControl';
 const serverURL = "";
 
 const theme = createTheme({
-  palette: {
-    type: "light",
-    background: {
-      default: "#FFFFFF",
+    palette: {
+        type: "light",
+        background: {
+            default: "#FFFFFF",
+        },
+        primary: {
+            main: "#ffd500",
+        },
+        secondary: {
+            main: "#be0002",
+        },
     },
-    primary: {
-      main: "#ffd500",
-    },
-    secondary: {
-      main: "#be0002",
-    },
-  },
 });
 
 export default function SignIn() {
-  const error1 = () =>
-    toast.error("Please fill out all required fields!", {
-      containerId: "error",
-    });
+    const error1 = () =>
+        toast.error("Please fill out all required fields!", {
+            containerId: "error",
+        });
 
-  const notifyAll = () =>
-    toast.success(<p>🎉Welcome to uwScoop, {firstName}!</p>, {
-      containerId: "success",
-    });
+    const failConfirmPassword = () =>
+        toast.error("Passwords do not match!", {
+            containerId: "error",
+        });
 
-  const [error, setError] = React.useState(false);
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [program, setProgram] = React.useState("");
-  const [year, setYear] = React.useState("");
-  const [birthday, setBirthday] = React.useState("");
-  const [phoneNumber, setPhoneNumber] = React.useState("");
-  const [music, setMusic] = React.useState("");
+    const notifyAll = () =>
+        toast.success(<p>🎉Welcome to uwScoop, {firstName}!</p>, {
+            containerId: "success",
+        });
 
-  const history = useHistory();
+    const [error, setError] = React.useState(false);
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [confirmPassword, setConfirmPassword] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [program, setProgram] = React.useState("");
+    const [year, setYear] = React.useState("");
+    const [birthday, setBirthday] = React.useState("");
+    const [phoneNumber, setPhoneNumber] = React.useState("");
+    const [music, setMusic] = React.useState("");
 
-  const callApiPostRequest = async () => {
-    const url = serverURL + "/api/registerUser";
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: password,
-        program: program,
-        year: year,
-        birthday: birthday,
-        phoneNumber: phoneNumber,
-        music: music,
-      }),
-    });
-    const body = await response.json();
-    if (response.status !== 200) {
-      throw Error(body.message);
-    }
-    return body;
-  };
+    const history = useHistory();
 
-  const redirectHome = () => {
-    history.push("/Home");
-  };
+    const callApiPostRequest = async () => {
+        const url = serverURL + "/api/registerUser";
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                password: password,
+                program: program,
+                year: year,
+                birthday: birthday,
+                phoneNumber: phoneNumber,
+                music: music,
+            }),
+        });
+        const body = await response.json();
+        if (response.status !== 200) {
+            throw Error(body.message);
+        }
+        return body;
+    };
 
-  const handleSubmit = (e) => {
-    e.persist();
-    if (
-      firstName == "" ||
-      lastName == "" ||
-      email == "" ||
-      password == "" ||
-      program == "" ||
-      year == "" ||
-      phoneNumber == "" ||
-      music == ""
-    ) {
-      setError(true);
-      error1();
-    } else {
-      callApiPostRequest();
-      console.log(firstName, lastName, password, email, program, year);
-      redirectHome();
-    }
-  };
+    const redirectHome = () => {
+        history.push("/Home");
+    };
 
-  return (
-    <MuiThemeProvider theme={theme}>
-      <div className={classes.root}>
-        <ToastContainer
-          enableMultiContainer
-          containerId={"error"}
-          toastStyle={{ color: "black" }}
-          position="top-center"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
-        <ToastContainer
-          enableMultiContainer
-          containerId={"A"}
-          toastStyle={{ color: "black" }}
-          position="top-center"
-          autoClose={2000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="light"
-        />
+    const handleSubmit = (e) => {
+        e.persist();
+        if (
+            firstName == "" ||
+            lastName == "" ||
+            email == "" ||
+            password == "" ||
+            program == "" ||
+            year == "" ||
+            phoneNumber == "" ||
+            music == ""
+        ) {
+            setError(true);
+            error1();
+        } else if (password != confirmPassword) {
+            setError(true)
+            failConfirmPassword();
+        }
+        else {
+            callApiPostRequest();
+            console.log(firstName, lastName, password, email, program, year);
+            redirectHome();
+        }
+    };
 
-        <CssBaseline />
-        <Paper
-          className={classes.paper}
-          style={{ border: "none", boxShadow: "none", overflow: "hidden" }}
-        >
-          <Card
-            style={{
-              border: "none",
-              boxShadow: "none",
-              height: "80vh",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <form>
-              <Typography
-                variant="h4"
-                style={{
-                  marginBottom: 20,
-                  fontStyle: "oblique",
-                  fontWeight: "bold",
-                }}
-              >
-                Sign up to uwScoop!
-              </Typography>
-              <div className="nameCols">
-                <TextField
-                  label="First Name"
-                  formControlProps={{
-                    fullWidth: true,
-                  }}
-                  type="text"
-                  onChange={(event) => setFirstName(event.target.value)}
+    return (
+        <MuiThemeProvider theme={theme}>
+            <div className={classes.root}>
+                <ToastContainer
+                    enableMultiContainer
+                    containerId={"error"}
+                    toastStyle={{ color: "black" }}
+                    position="top-center"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
                 />
-                <TextField
-                  label="Last Name"
-                  formControlProps={{
-                    fullWidth: true,
-                  }}
-                  type="text"
-                  onChange={(event) => setLastName(event.target.value)}
+                <ToastContainer
+                    enableMultiContainer
+                    containerId={"A"}
+                    toastStyle={{ color: "black" }}
+                    position="top-center"
+                    autoClose={2000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
                 />
-              </div>
-              <br />
-              <TextField
-                label="Email"
-                id="email"
-                formControlProps={{
-                  fullWidth: true,
-                }}
-                type="text"
-                onChange={(event) => setEmail(event.target.value)}
-              />
-              <br />
-              <TextField
-                label="Password"
-                id="password"
-                formControlProps={{
-                  fullWidth: true,
-                }}
-                type="password"
-                onChange={(event) => setPassword(event.target.value)}
-              />
-              <br />
-              <div className="nameCols">
-                <TextField
-                  label="Phone Number"
-                  id="names"
-                  formControlProps={{
-                    fullWidth: true,
-                  }}
-                  type="text"
-                  onChange={(event) => setPhoneNumber(event.target.value)}
-                />
-                <FormControl sx={{ minWidth: 120 }}>
-                <InputLabel >Music Preference</InputLabel>
-                <Select value={music} label="Music Preference" onChange={(event) => setMusic(event.target.value)}>
-                  <MenuItem value={"Country"}>Country</MenuItem>
-                  <MenuItem value={"Classical"}>Classical</MenuItem>
-                  <MenuItem value={"Jazz"}>Jazz</MenuItem>
-                  <MenuItem value={"Latin"}>Latin</MenuItem>
-                  <MenuItem value={"Pop"}>Pop</MenuItem>
-                  <MenuItem value={"Rap"}>Rap</MenuItem>
-                  <MenuItem value={"R&B"}>R&B</MenuItem>
-                </Select>
-                </FormControl>
-              </div>
-              <br />
-              <div className="dateCols">
-                <TextField
-                  label="Program"
-                  id="names"
-                  formControlProps={{
-                    fullWidth: true,
-                  }}
-                  type="text"
-                  onChange={(event) => setProgram(event.target.value)}
-                />
-                <TextField
-                  label="Year"
-                  id="names"
-                  type="text"
-                  onChange={(event) => setYear(event.target.value)}
-                />
-              </div>
-              <br />
-              <TextField
-                label="Birthday"
-                id="names"
-                type="text"
-                onChange={(event) => setBirthday(event.target.value)}
-              />
-              <br />
-              <Button
-                variant="contained"
-                startIcon={<StartIcon />}
-                sx={{
-                  marginTop: 2,
-                  backgroundColor: "#ffd500",
-                  color: "black",
-                  "&:hover": {
-                    backgroundColor: "#ffd500",
-                    color: "#be0002",
-                  },
-                  fontWeight: "bold",
-                }}
-                onClick={handleSubmit}
-              >
-                Create Account
-              </Button>
-            </form>
-          </Card>
-        </Paper>
-      </div>
-    </MuiThemeProvider>
-  );
+
+                <CssBaseline />
+                <Paper
+                    className={classes.paper}
+                    style={{ border: "none", boxShadow: "none", overflow: "hidden" }}
+                >
+                    <Card
+                        style={{
+                            border: "none",
+                            boxShadow: "none",
+                            height: "80vh",
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <form>
+                            <Typography
+                                variant="h4"
+                                style={{
+                                    marginBottom: 20,
+                                    fontStyle: "oblique",
+                                    fontWeight: "bold",
+                                }}
+                            >
+                                Sign up to uwScoop!
+                            </Typography>
+                            <div className="nameCols">
+                                <TextField
+                                    label="First Name"
+                                    formControlProps={{
+                                        fullWidth: true,
+                                    }}
+                                    type="text"
+                                    onChange={(event) => setFirstName(event.target.value)}
+                                />
+                                <TextField
+                                    label="Last Name"
+                                    formControlProps={{
+                                        fullWidth: true,
+                                    }}
+                                    type="text"
+                                    onChange={(event) => setLastName(event.target.value)}
+                                />
+                            </div>
+                            <br />
+                            <TextField
+                                label="Phone Number"
+                                id="names"
+                                formControlProps={{
+                                    fullWidth: true,
+                                }}
+                                type="text"
+                                onChange={(event) => setPhoneNumber(event.target.value)}
+                            />
+                            <br />
+                            <TextField
+                                label="Email"
+                                id="email"
+                                formControlProps={{
+                                    fullWidth: true,
+                                }}
+                                type="text"
+                                onChange={(event) => setEmail(event.target.value)}
+                            />
+                            <br />
+                            <TextField
+                                label="Password"
+                                id="password"
+                                formControlProps={{
+                                    fullWidth: true,
+                                }}
+                                type="password"
+                                onChange={(event) => setPassword(event.target.value)}
+                            />
+                            <br />
+                            <TextField
+                                label="Confirm Password"
+                                id="password"
+                                formControlProps={{
+                                    fullWidth: true,
+                                }}
+                                type="password"
+                                onChange={(event) => setConfirmPassword(event.target.value)}
+                            />
+                            <br />
+                            <hr style={{ backgroundColor: "#ffd500", height: "2px", border: "none" }} />
+                            <br />
+
+
+
+                            <div className="dateCols">
+                                <TextField
+                                    label="Program"
+                                    id="names"
+                                    formControlProps={{
+                                        fullWidth: true,
+                                    }}
+                                    type="text"
+                                    onChange={(event) => setProgram(event.target.value)}
+                                />
+                                <TextField
+                                    label="Year"
+                                    id="names"
+                                    type="text"
+                                    onChange={(event) => setYear(event.target.value)}
+                                />
+                            </div>
+                            <br />
+                            <div className="nameCols">
+                                <TextField
+                                    label="Birthday"
+                                    id="names"
+                                    type="text"
+                                    onChange={(event) => setBirthday(event.target.value)}
+                                />
+                                <FormControl sx={{ minWidth: 120 }}>
+                                    <InputLabel >Music Preference</InputLabel>
+                                    <Select value={music} label="Music Preference" onChange={(event) => setMusic(event.target.value)}>
+                                        <MenuItem value={"Country"}>Country</MenuItem>
+                                        <MenuItem value={"Classical"}>Classical</MenuItem>
+                                        <MenuItem value={"Jazz"}>Jazz</MenuItem>
+                                        <MenuItem value={"Latin"}>Latin</MenuItem>
+                                        <MenuItem value={"Pop"}>Pop</MenuItem>
+                                        <MenuItem value={"Rap"}>Rap</MenuItem>
+                                        <MenuItem value={"R&B"}>R&B</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+
+                            <br />
+                            <br />
+                            <Button
+                                variant="contained"
+                                startIcon={<StartIcon />}
+                                sx={{
+                                    marginTop: 2,
+                                    backgroundColor: "#ffd500",
+                                    color: "black",
+                                    "&:hover": {
+                                        backgroundColor: "#ffd500",
+                                        color: "#be0002",
+                                    },
+                                    fontWeight: "bold",
+                                }}
+                                onClick={handleSubmit}
+                            >
+                                Create Account
+                            </Button>
+                        </form>
+                    </Card>
+                </Paper>
+            </div>
+        </MuiThemeProvider>
+    );
 }
